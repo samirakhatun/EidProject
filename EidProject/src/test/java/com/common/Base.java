743228f15987334d183bfec8327e3104901b60ae
samirakhatun <samirakhatun@10.67.111.25>
2019-09-03 19:23:00 -0400

    first commit

---
+++ EidProject/src/test/java/com/common/Base.java
@@ -0,0 +1,22 @@
+package com.common;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	
+public WebDriver driver;
+
+public WebDriver getdriver() {
+	
+System.setProperty("webdriver.chrome.driver","./Driver/chromedriver");
+driver=new ChromeDriver();
+driver.get("https://www.facebook.com");
+driver.manage().window().maximize();
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+return driver;
+
+}
+}
